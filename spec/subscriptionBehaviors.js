5da82da325c05d157f8e1de29a44cface8cefcf5
Timothy Farrell <explorigin@gmail.com>
2013-06-26 22:32:58 -0500

    Split one test into to for more granularity.

--- spec/subscriptionBehaviors.js
+++ spec/subscriptionBehaviors.js
@@ -17,15 +17,9 @@ describe('Subscription ', function() {
             shelly = new Intern(),
             output = null;
 
-        expect(shelly._subscriptions.length).toEqual(0);
-        expect(shelly._attrSubscriptions.length).toEqual(0);
-
         shelly.subscribe(function(value) {
             output = value;
         });
-
-        expect(shelly._subscriptions.length).toEqual(1);
-        expect(shelly._attrSubscriptions.length).toEqual(1);
 
         runs(function() {
             shelly.set({first_name: 'Shelly'});
@@ -39,11 +33,39 @@ describe('Subscription ', function() {
             expect(output).toEqual({
                 first_name: "Shelly"
             });
-
-            shelly.destroy();
-
-            expect(shelly._subscriptions.length).toEqual(0);
-            expect(shelly._attrSubscriptions.length).toEqual(0);
         });
+    });
+
+    it('Should clean up all subscriptions when disposed', function () {
+        var Intern = ko.Model.extend({
+                initialize: function () {
+                    var self = this;
+
+                    this.first_name = ko.observable();
+                    this.last_name = ko.observable();
+                    this.age = null;
+                    this.name = ko.computed(function () {
+                        return self.first_name() + " " + self.last_name();
+                    });
+                },
+                subscriptionParameters: ['first_name']
+            }),
+            shelly = new Intern(),
+            output = null;
+
+        expect(shelly._subscriptions.length).toEqual(0);
+        expect(shelly._attrSubscriptions.length).toEqual(0);
+
+        shelly.subscribe(function(value) {
+            output = value;
+        });
+
+        expect(shelly._subscriptions.length).toEqual(1);
+        expect(shelly._attrSubscriptions.length).toEqual(1);
+
+        shelly.destroy();
+
+        expect(shelly._subscriptions.length).toEqual(0);
+        expect(shelly._attrSubscriptions.length).toEqual(0);
     });
 });
